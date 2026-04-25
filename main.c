0372d594ce139f81b9e06e2efc47a20b28d75c36
farid45219 <refrigerator.rni.eed7@waltonplc.com>
2026-04-25 18:50:37 +0600

    Modified: scheduler, manual task switch

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(void) {
 	Kernel_Init();
     Kernel_Task_Create(Tasks_Task1,0);
     Kernel_Task_Create(Tasks_Task2,1);
-	Kernel_Task_Create(Tasks_Task3,2);
+	//Kernel_Task_Create(Tasks_Task3,2);
 	Kernel_Start_Tasks();
 	
     while(1){
